1be304a9a671291a66153a05e6550ad4bcc7c7fd
Janusz Piechówka <januszpiechowka@gmail.com>
2020-02-03 11:39:46 +0100

    Report all missing headers in all responses (4XX, 500 etc.), create separate issues for different responses, update gradle plugins, bump version to 1.1.0.

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class BurpExtender implements IBurpExtender, IScannerCheck {
     private static final String EXTENSION_NAME = "Burp Security Headers Checker";
-    private static final String EXTENSION_VERSION = "1.0.0";
+    private static final String EXTENSION_VERSION = "1.1.0";
 
     private IBurpExtenderCallbacks burpExtenderCallbacks;
     private IExtensionHelpers burpExtensionHelpers;
@@ -31,54 +31,76 @@ public class BurpExtender implements IBurpExtender, IScannerCheck {
 
     @Override
     public List<IScanIssue> doPassiveScan(IHttpRequestResponse baseRequestResponse) {
-        val scanIssuesList = new ArrayList<IScanIssue>();
-
-        val response = baseRequestResponse.getResponse();
-        val responseInfo = this.burpExtensionHelpers.analyzeResponse(response);
-
-        // Check if the response code is lower than 400 then proceed
-        if (responseInfo.getStatusCode() < 400) {
-            val responseHeaders = responseInfo.getHeaders();
-
-            for (CheckedSecurityHeadersEnum headerToCheck : CheckedSecurityHeadersEnum.values()) {
-                boolean containsCheckedHeader = false;
-
-                for (String responseHeader : responseHeaders) {
-                    if (responseHeader.toLowerCase().contains(headerToCheck.getHeaderName().toLowerCase())) {
-                        containsCheckedHeader = true;
-                        break;
-                    }
-                }
-
-                if (!containsCheckedHeader) {
-                    val missingHeadersSanIssue = new MissingSecurityHeaderIssue(
-                            this.burpExtensionHelpers.analyzeRequest(baseRequestResponse).getUrl(),
-                            "Missing Security Header: " + headerToCheck.getHeaderName(),
-                            "No " + headerToCheck.getHeaderName() + " security header has been detected in the server responses.",
-                            new IHttpRequestResponse[]{this.burpExtenderCallbacks.applyMarkers(baseRequestResponse, null, null)},
-                            baseRequestResponse.getHttpService()
-                    );
-
-                    scanIssuesList.add(missingHeadersSanIssue);
-                }
-
-            }
-            return scanIssuesList;
-        } else {
-            return Collections.emptyList();
-        }
+        return checkResponseSecurityHeaders(baseRequestResponse);
     }
+
 
     @Override
     public List<IScanIssue> doActiveScan(IHttpRequestResponse baseRequestResponse, IScannerInsertionPoint insertionPoint) {
         return Collections.emptyList();
     }
 
+
     @Override
     public int consolidateDuplicateIssues(IScanIssue existingIssue, IScanIssue newIssue) {
         if (existingIssue.getIssueName().equalsIgnoreCase(newIssue.getIssueName()))
             return -1; // Return old issue
         else return 0;
+    }
+
+
+    private List<IScanIssue> checkResponseSecurityHeaders(IHttpRequestResponse baseRequestResponse) {
+        val response = baseRequestResponse.getResponse();
+        val responseInfo = this.burpExtensionHelpers.analyzeResponse(response);
+
+        val scanIssuesList = new ArrayList<IScanIssue>();
+
+        for (CheckedSecurityHeadersEnum headerToCheck : CheckedSecurityHeadersEnum.values()) {
+            boolean containsCheckedHeader = false;
+
+            for (String responseHeader : responseInfo.getHeaders()) {
+                if (responseHeader.toLowerCase().contains(headerToCheck.getHeaderName().toLowerCase())) {
+                    containsCheckedHeader = true;
+                    break;
+                }
+            }
+
+            if (!containsCheckedHeader) {
+                val missingHeadersScanIssue = createNewScannerIssue(responseInfo.getStatusCode(), headerToCheck, baseRequestResponse);
+                scanIssuesList.add(missingHeadersScanIssue);
+            }
+        }
+
+        return scanIssuesList;
+    }
+
+
+    private MissingSecurityHeaderIssue createNewScannerIssue(int statusCode, CheckedSecurityHeadersEnum headerToCheck, IHttpRequestResponse baseRequestResponse) {
+        String issueName = "";
+
+        if (statusCode < 400) {
+            issueName = "Missing Security Header: " + headerToCheck.getHeaderName();
+        }
+
+        if (statusCode >= 400 && statusCode < 500) {
+            issueName = "Missing Security Header in 4XX response: " + headerToCheck.getHeaderName();
+        }
+
+        if (statusCode == 500) {
+            issueName = "Missing Security Header in 500 response: " + headerToCheck.getHeaderName();
+        }
+
+        if (statusCode > 500) {
+            issueName = "Missing Security Header in other response: " + headerToCheck.getHeaderName();
+        }
+
+        return new MissingSecurityHeaderIssue(
+                this.burpExtensionHelpers.analyzeRequest(baseRequestResponse).getUrl(),
+                issueName,
+                "No " + headerToCheck.getHeaderName() + " security header has been detected in the server responses.",
+                new IHttpRequestResponse[]{this.burpExtenderCallbacks.applyMarkers(baseRequestResponse, null, null)},
+                baseRequestResponse.getHttpService()
+        );
     }
 
 }
